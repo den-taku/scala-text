1b75acf9f612c95fe55ee5eda51600c30da4645e
DenTaku <dentaku1412@gmail.com>
2021-01-30 13:58:59 +0900

    Learn 'ケースクラスとパターンマッチ'

--- src/train01.scala
+++ src/train01.scala
@@ -1276,3 +1276,125 @@ s -= 5
 
 s
 // res55: scala.collection.mutable.Set[Int] = Set(1, 2, 3, 4)
+
+sealed abstract class DayOfWeek
+case object Sunday extends DayOfWeek
+case object Monday extends DayOfWeek
+case object Tuesday extends DayOfWeek
+case object Wednesday extends DayOfWeek
+case object Thursday extends DayOfWeek
+case object Friday extends DayOfWeek
+case object Saturday extends DayOfWeek
+// defined class DayOfWeek
+// defined object Sunday
+// defined object Monday
+// defined object Tuesday
+// defined object Wednesday
+// defined object Thursday
+// defined object Firday
+// defined object Saturday
+
+val x: DayOfWeek = Sunday
+// x: DayOfWeek = Sunday
+
+x match {
+    case Sunday => 1
+    case Monday => 2
+    case Tuesday => 3
+    case Wednesday => 4
+    case Thursday => 5
+    case Friday => 6 
+}
+
+sealed abstract class Exp
+case class Add(lhs: Exp, rhs: Exp) extends Exp
+case class Sub(lhs: Exp, rhs: Exp) extends Exp
+case class Mul(lhs: Exp, rhs: Exp) extends Exp
+case class Div(lhs: Exp, rhs: Exp) extends Exp
+case class Lit(value: Int) extends Exp
+
+val example = Add(Lit(1), Div(Mul(Lit(2), Lit(3)), Lit(2)))
+// example: Add = Add(Lit(1),Div(Mul(Lit(2),Lit(3)),Lit(2)))
+
+def eval(exp: Exp): Int = exp match {
+    case Add(l, r) => eval(l) + eval(r)
+    case Sub(l, r) => eval(l) - eval(r)
+    case Mul(l, r) => eval(l) * eval(r)
+    case Div(l, r) => eval(l) / eval(r)
+    case Lit(v) => v
+}
+// eval: (exp: Exp)Int
+
+eval(example)
+// res1: Int = 4
+
+case class Point(x: Int, y: Int)
+// defined class Point
+
+val Point(x, y) = Point(10, 20)
+// x: Int = 10
+// y: Int = 20
+
+val p = Point(1, 2)
+// p: Point = Point(1,2)
+
+println(p.x, p.y)
+// (1,2)
+
+Point(1, 2) == Point(1, 2)
+// res3: Boolean = true
+
+Point(1, 2) == Point(3,4)
+// res4: Boolean = false
+
+class Point(val x: Int, val y: Int) {
+    override def equals(that: Any): Boolean = that match {
+        case thatPoint: Point => 
+            thatPoint.canEqual(this) && this.x == thatPoint.x && this.y == thatPoint.y
+        case _ =>
+            false
+    }
+
+    override def hashCode(): Int = x.hashCode ^ y.hashCode
+
+    def canEqual(that: Any): Boolean = that.isInstanceOf[Point]
+
+    override def toString(): String = "Point(" + x + ", " + y + ")"
+}
+// defined class Point
+
+object Point {
+    def apply(x: Int, y: Int): Point = new Point(x, y)
+}
+// defined object Point
+
+// DayOfWeek型を使って、ある日の次の曜日を返すメソッドnextDayOfWeek
+// 
+// def nextDayOfWeek(d: DayOfWeek): DayOfWeek = ???
+// 
+// をパターンマッチを用いて定義してみましょう。
+def nextDayOfWeek(d: DayOfWeek): DayOfWeek = d match {
+    case Sunday => Monday
+    case Monday => Tuesday
+    case Tuesday => Wednesday
+    case Wednesday => Thursday
+    case Thursday => Friday
+    case Friday => Saturday
+    case Saturday => Sunday
+}
+// nextDayOfWeek: (d: DayOfWeek)DayOfWeek
+
+List(1, 2, 3, 4, 5).collect { case i if i % 2 == 1 => i * 2 }
+// res5: List[Int] = List(2, 6, 10)
+
+val even: Int => Boolean = {
+    case i if i % 2 == 0 => true
+    case _ => false
+}
+// even: Int => Boolean = $Lambda$4964/0x0000000801adeea0@3091dd92
+
+val even: Int => Boolean = (x => x match {
+    case i if i % 2 == 0 => true
+    case _ => false
+})
+// even: Int => Boolean = $Lambda$4967/0x0000000801adfad0@17516840
